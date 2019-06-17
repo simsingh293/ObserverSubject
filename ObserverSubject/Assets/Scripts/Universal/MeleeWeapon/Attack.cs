ebef898be42f041d165102dd1b1411cbb4b726e5
simsingh293 <30273904+simsingh293@users.noreply.github.com>
2019-06-17 19:06:11 -0400

    Melee Functionality

---
+++ ObserverSubject/Assets/Scripts/Universal/MeleeWeapon/Attack.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    string _AttackName;
+    public Animation _AttackAnimation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ObserverSubject/Assets/Scripts/Universal/MeleeWeapon/MeleeWeapon.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeWeapon : MonoBehaviour
+{
+    [Header("Weapon Attributes")]
+    public float _AttackPower = 10.0f;
+
+    [Header("Weapon Parts")]
+    public SwordManager _Manager;
+    public Animator _anim;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Attack();
+    }
+
+    void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            _anim.SetTrigger("Swing1");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            _anim.SetTrigger("Swing2");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            _anim.SetTrigger("Swing3");
+        }
+    }
+}
